ec70db708cd4bbcc6d7d90beeec6c488670958f0
Asharafraza Desai <132386307+AshrafDesai@users.noreply.github.com>
2023-09-06 18:18:54 +0530

    reverseInKGroup.cpp

--- Lecture046 Linked List Day3/reverseInKGroup.cpp
+++ Lecture046 Linked List Day3/reverseInKGroup.cpp
@@ -14,8 +14,6 @@
     };
 
 *****************************************************************/
-
-
 Node* kReverse(Node* head, int k) {
     //base call
     if(head == NULL) {
@@ -27,20 +25,35 @@ Node* kReverse(Node* head, int k) {
     Node* curr = head;
     Node* prev = NULL;
     int count= 0;
-    
-    while( curr != NULL && count < k ) {
-        next = curr -> next;
-        curr -> next = prev;
-        prev = curr;
-        curr = next;
+    Node* temp = head;
+    while (temp != nullptr && count < k) 
+    {
+        temp = temp->next;
         count++;
     }
+    if(count == k)
+    {
+        count = 0;
+        while(curr != NULL && count < k)
+        {
+            next = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = next;
+            count++;
+        }
     
-    //step2: Recursion dekhlega aage ka 
-    if(next != NULL) {
-        head -> next = kReverse(next,k);
+    //step2: Recursion dekhlega aage ka
+        if (next != NULL) 
+        {
+            head->next = kReverse(next, k);
+        }
+
+        // step3: return head of reversed list
+        return prev;
+        }
+        else
+        {
+            return head;
+        }
     }
-    
-    //step3: return head of reversed list
-    return prev;
-}
