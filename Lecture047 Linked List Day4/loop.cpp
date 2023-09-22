18c19e3f3b0ea69bd4448578d615677b77523f25
Asharafraza Desai <132386307+AshrafDesai@users.noreply.github.com>
2023-09-22 16:36:37 +0530

    loop.cpp

--- Lecture047 Linked List Day4/loop.cpp
+++ Lecture047 Linked List Day4/loop.cpp
@@ -13,70 +13,59 @@
     };
 
 *************************************************/
-
 Node* floydDetectLoop(Node* head) {
-
-    if(head == NULL)
+    if (head == NULL)
         return NULL;
 
     Node* slow = head;
     Node* fast = head;
 
-    while(slow != NULL && fast !=NULL) {
-        
-        fast = fast -> next;
-        if(fast != NULL) {
-            fast = fast -> next;
-        }
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
 
-        slow = slow -> next;
-
-        if(slow == fast) {
+        if (slow == fast) {
             return slow;
         }
     }
 
     return NULL;
-
 }
 
 Node* getStartingNode(Node* head) {
-
-    if(head == NULL) 
+    if (head == NULL)
         return NULL;
 
     Node* intersection = floydDetectLoop(head);
-    
-    if(intersection == NULL)
+
+    if (intersection == NULL)
         return NULL;
-    
+
     Node* slow = head;
 
-    while(slow != intersection) {
-        slow = slow -> next;
-        intersection = intersection -> next;
-    }  
+    while (slow != intersection) {
+        slow = slow->next;
+        intersection = intersection->next;
+    }
 
     return slow;
-
 }
 
-Node *removeLoop(Node *head)
-{
-    if( head == NULL)
+Node* removeLoop(Node* head) {
+    if (head == NULL)
         return NULL;
 
     Node* startOfLoop = getStartingNode(head);
-    
-    if(startOfLoop == NULL)
+
+    if (startOfLoop == NULL)
         return head;
-    
+
     Node* temp = startOfLoop;
 
-    while(temp -> next != startOfLoop) {
-        temp = temp -> next;
-    } 
+    while (temp->next != startOfLoop) {
+        temp = temp->next;
+    }
 
-    temp -> next = NULL;
+    temp->next = NULL;
     return head;
 }
